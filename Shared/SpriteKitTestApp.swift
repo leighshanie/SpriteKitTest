e641e82c3deaaee73653bf66f5669d83a1dffa40
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-05 17:00:04 +0800

    Initial Commit

---
+++ Shared/SpriteKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitTestApp.swift
+//  Shared
+//
+//  Created by Shane Leigh on 05/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
